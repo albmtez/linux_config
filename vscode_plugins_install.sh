2e08439cd008b4a7a4282c2f6e08b1352809e2a3
Alberto Martinez <albmtez@users.noreply.github.com>
2018-11-18 00:53:00 +0100

    Added script to install vscode plugins

---
+++ vscode_plugins_install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+code --install-extension ms-vscode.go
+code --install-extension peterjausovec.vscode-docker
+code --install-extension rogalmic.bash-debug
+code --install-extension johnstoncode.svn-scm
+code --install-extension bbenoist.vagrant
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension eamodio.gitlens
+code --install-extension alefragnani.project-manager
+code --install-extension quicktype.quicktype
+code --install-extension alefragnani.bookmarks
+code --install-extension ritwickdey.liveserver
+code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension formulahendry.code-runner
+code --install-extension deerawan.vscode-faker
+code --install-extension ms-python.python
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+code --install-extension GitHub.vscode-pull-request-github
+code --install-extension ms-vscode.csharp
+code --install-extension dotjoshjohnson.xml
+code --install-extension vscjava.vscode-java-pack
+code --install-extension loiane.angular-extension-pack
