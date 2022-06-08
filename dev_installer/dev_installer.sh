c48a0a9cf7cd50238c6a88c7405847915b03e289
Alberto Martínez <alb.mtez@gmail.com>
2022-06-08 11:19:26 +0200

    Docker engine install updated

--- dev_installer/dev_installer.sh
+++ dev_installer/dev_installer.sh
@@ -346,22 +346,30 @@ function docker_install {
   # Uninstall old versions.
   sudo apt remove -y docker docker-engine docker.io containerd runc
 
-  # Install packages to allow apt to use a repository over HTTPS.
+  # Install the required packages to allow apt to use a repository over HTTPS
   sudo apt update
-  sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+  sudo apt install ca-certificates curl gnupg lsb-release
 
-  # Add GPG key.
-  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+  # Add Docker's official GPG key
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-  # Configure apt repository.
-  sudo add-apt-repository \
-        "deb [arch=amd64] https://download.docker.com/linux/debian \
-        $(lsb_release -cs) \
-        stable"
+  echo $DISTRO
+  # Set up the repository
+  if [ $DISTRO = "Ubuntu" ]; then
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  fi
+  if [ $DISTRO = "Debian" ]; then
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  fi
 
-  # Install the latest version of Docker Engine - Community and containerd.
-  sudo apt update
-  sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+  # Install Docker engine
+  sudo apt-get update
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
   # Add the user to docker group.
   sudo usermod -aG docker $(whoami)
